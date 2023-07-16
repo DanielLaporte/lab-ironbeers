ccb756d1986fe89aa1dd715a3133d436bcb55f63
DanielLaporte <daniel.laporte2008@gmail.com>
2023-07-16 16:31:14 +0200

    terminado

--- app.js
+++ app.js
@@ -12,14 +12,32 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => {
+      console.log(error);
+      res.render('error');
+    });
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beerFromApi => {
+      res.render('random-beer', { beer: beerFromApi[0] });
+    })
+    .catch(error => {
+      console.log(error);
+      res.render('error');
+    });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
